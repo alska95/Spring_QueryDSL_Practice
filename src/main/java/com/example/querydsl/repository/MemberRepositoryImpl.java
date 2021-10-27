ba290742baf484ebe2dc649d61e93aabb5b2e12d
alska95 <rhsalska55@naver.com>
2021-10-28 06:57:48 +0900

    paging 기법, count 쿼리 최적화

--- src/main/java/com/example/querydsl/repository/MemberRepositoryImpl.java
+++ src/main/java/com/example/querydsl/repository/MemberRepositoryImpl.java
@@ -4,10 +4,14 @@ import com.example.querydsl.dto.MemberSearchCondition;
 import com.example.querydsl.dto.MemberTeamDto;
 
 import com.example.querydsl.dto.QMemberTeamDto;
-import com.example.querydsl.entity.QMember;
-import com.example.querydsl.entity.QTeam;
+import com.querydsl.core.QueryResults;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.util.StringUtils;
 
 import java.util.List;
@@ -35,6 +39,74 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom{
                         ,teamNameEq(condition.getTeamName())
                 )
                 .fetch();
+    }
+
+    @Override
+    public Page<MemberTeamDto> searchPageSimple(MemberSearchCondition condition, Pageable pageable) {
+        QueryResults<MemberTeamDto> result = queryFactory.select(new QMemberTeamDto(member.id.as("memberId"), member.name, member.age, team.id.as("teamId"), team.name.as("teamName")))
+                .from(member)
+                .leftJoin(member.team(), team)
+                .where(
+                        ageGoe(condition.getAgeGoe())
+                        , ageLoe(condition.getAgeLoe())
+                        , nameEq(condition.getName())
+                        , teamNameEq(condition.getTeamName())
+                )
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetchResults();//fetchResult로 하면, count쿼리랑 content쿼리 둘다 날린다.
+
+        return new PageImpl<>(result.getResults(),pageable , result.getTotal());
+
+        /**
+         * totalCount쓸때는 orderBy같은거 쓰지 말기!(count성능에 영향을 준다.)
+         **/
+    }
+
+    @Override
+    public Page<MemberTeamDto> searchPageComplex(MemberSearchCondition condition, Pageable pageable) {
+        QueryResults<MemberTeamDto> result = queryFactory.select(
+                new QMemberTeamDto(member.id.as("memberId"), member.name
+                        , member.age, team.id.as("teamId")
+                        , team.name.as("teamName")))
+                .from(member)
+                .leftJoin(member.team(), team)
+                .where(
+                        ageGoe(condition.getAgeGoe())
+                        , ageLoe(condition.getAgeLoe())
+                        , nameEq(condition.getName())
+                        , teamNameEq(condition.getTeamName())
+                )
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetchResults();//fetchResult로 하면, count쿼리랑 content쿼리 둘다 날린다.
+
+        JPAQuery<MemberTeamDto> countQuery = queryFactory.select(new QMemberTeamDto(
+                member.id.as("memberId"), member.name
+                , member.age, team.id.as("teamId")
+                , team.name.as("teamName")))
+                .from(member)
+                .leftJoin(member.team(), team)
+                .where(
+                        ageGoe(condition.getAgeGoe())
+                        , ageLoe(condition.getAgeLoe())
+                        , nameEq(condition.getName())
+                        , teamNameEq(condition.getTeamName())
+                );
+
+        PageableExecutionUtils.getPage(result.getResults() , pageable , countQuery::fetchCount);
+        //content와 pagable 토탈 사이즈를 보고 람다식을 생략할 수 있으면 생략한다.
+        /**
+         * 쿼리를 분류해서 count쿼리는 따로 날린다.
+         * count쿼리를 왜 따로 짜지??
+         * join이 필요 없는 등 count쿼리를 쉽게 짤 수 있는 경우가 많다. --> 성능의 차이를 야기함 (걍 select로 하면 조인 등등 다붙음)
+         *
+         * 카운트 쿼리를 생략할 수도 있다.
+         *  페이지 시작하면서 컨텐츠 사이즈가 페이지 사이즈보다 작은경우
+         *      마지막 페이지 일 경우(offext + 컨텐츠 사이즈를 더해서 전체 사이즈 구한다.)
+         * */
+
+        return new PageImpl<>(result.getResults(),pageable , countQuery.fetchCount());
     }
 
     public BooleanExpression ageGoe(Integer ageGoe){
--- src/test/java/com/example/querydsl/repository/MemberRepositoryTest.java
+++ src/test/java/com/example/querydsl/repository/MemberRepositoryTest.java
@@ -7,6 +7,8 @@ import com.example.querydsl.entity.Team;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
@@ -70,7 +72,36 @@ class MemberRepositoryTest {
         for (MemberTeamDto memberTeamDto : memberTeamDtos) {
             System.out.println("memberTeamDto = " + memberTeamDto);
         }
+    }
+    @Test
+    public void searchPageSimple(){
 
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+//
+        MemberSearchCondition condition = new MemberSearchCondition();
+//        condition.setAgeGoe(1);
+//        condition.setAgeLoe(100);
+//        condition.setName("member1");
+
+        PageRequest page = PageRequest.of(0, 3);
+        Page<MemberTeamDto> memberTeamDtos = memberRepository.searchPageSimple(condition, page);
+        for (MemberTeamDto memberTeamDto : memberTeamDtos) {
+            System.out.println("memberTeamDto = " + memberTeamDto);
+        }
     }
 
 }
